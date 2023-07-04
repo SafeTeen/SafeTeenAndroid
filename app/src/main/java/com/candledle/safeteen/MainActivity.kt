853ecb35ed7b24ddd0e3cd487b12be66abc671ba
Tmdhoon2 <hks026naver@gmail.com>
2023-07-04 20:48:14 +0900

    feat :: implement main bottom navigation screen

--- app/src/main/java/com/candledle/safeteen/MainActivity.kt
+++ app/src/main/java/com/candledle/safeteen/MainActivity.kt
@@ -4,12 +4,11 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.candledle.safeteen.design_system.theme.SafeteenTheme
+import com.candledle.safeteen.feature.home.MainScreen
 import com.candledle.safeteen.feature.landing.LandingScreen
 import com.candledle.safeteen.feature.signin.SignInScreen
 import com.candledle.safeteen.feature.signup.SignUpScreen
@@ -17,7 +16,6 @@ import com.candledle.safeteen.navigation.SafeNavigation
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,28 +23,32 @@ class MainActivity : ComponentActivity() {
 
                 val navHostController = rememberNavController()
 
-                Scaffold {
-                    NavHost(
-                        navController = navHostController,
-                        startDestination = SafeNavigation.Landing,
+                NavHost(
+                    navController = navHostController,
+                    startDestination = SafeNavigation.Landing,
+                ) {
+                    composable(
+                        route = SafeNavigation.Landing,
                     ) {
-                        composable(
-                            route = SafeNavigation.Landing,
-                        ) {
-                            LandingScreen(navController = navHostController)
-                        }
+                        LandingScreen(navController = navHostController)
+                    }
 
-                        composable(
-                            route = SafeNavigation.SignIn,
-                        ){
-                            SignInScreen(navController = navHostController)
-                        }
+                    composable(
+                        route = SafeNavigation.SignIn,
+                    ) {
+                        SignInScreen(navController = navHostController)
+                    }
 
-                        composable(
-                            route = SafeNavigation.SignUp,
-                        ){
-                            SignUpScreen(navController = navHostController)
-                        }
+                    composable(
+                        route = SafeNavigation.SignUp,
+                    ) {
+                        SignUpScreen(navController = navHostController)
+                    }
+
+                    composable(
+                        route = SafeNavigation.Main,
+                    ){
+                        MainScreen()
                     }
                 }
             }
