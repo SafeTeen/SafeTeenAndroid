179fa3d8d1e5fc86668fac51e48974cec869fa3a
Tmdhoon2 <hks026naver@gmail.com>
2023-07-04 15:56:45 +0900

    feat :: Safeteen color

--- app/src/main/java/com/candledle/safeteen/ui/theme/Color.kt
+++ app/src/main/java/com/candledle/safeteen/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.candledle.safeteen.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+object SafeColor{
+    val Main100 = Color(0xFFD3FFEF)
+    val Main300 = Color(0xFF8DFDD5)
+    val Main500 = Color(0xFF22F6AA)
+    val Main700 = Color(0xFF10DF94)
+    val Main900 = Color(0xFF09AC72)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+    val White = Color(0xFFFFFFFF)
+    val Gray100 = Color(0xFFFAFAFA)
+    val Gray300 = Color(0xFFF6F6F6)
+    val Gray500 = Color(0xFFDEDEDE)
+    val Gray600 = Color(0xFFD9D9D9)
+    val Gray700 = Color(0xFF8D8D8D)
+    val Gray800 = Color(0xFF424242)
+    val Gray900 = Color(0xFF2F2F2F)
+    val Black = Color(0xFF000000)
+}
