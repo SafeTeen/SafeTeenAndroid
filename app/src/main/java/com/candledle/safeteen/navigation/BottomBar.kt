96c65df3a40c8c621284d7cfd08de53141948b94
Tmdhoon2 <hks026naver@gmail.com>
2023-07-05 22:06:32 +0900

    refactor :: bottombar padding

--- app/src/main/java/com/candledle/safeteen/navigation/BottomBar.kt
+++ app/src/main/java/com/candledle/safeteen/navigation/BottomBar.kt
@@ -1,7 +1,17 @@
 package com.candledle.safeteen.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
@@ -10,6 +20,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -17,6 +29,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.candledle.safeteen.design_system.theme.Body4
 import com.candledle.safeteen.design_system.theme.SafeColor
+import com.google.accompanist.insets.LocalWindowInsets
 
 
 @Composable
@@ -33,51 +46,64 @@ internal fun BottomBar(
 
     val navBackStackEntry by navController.currentBackStackEntryAsState()
 
-    BottomNavigation(
-        modifier = Modifier
-            .height(68.dp)
-            .graphicsLayer(
-                clip = true,
-                shape = RoundedCornerShape(
-                    topStart = 16.dp,
-                    topEnd = 16.dp,
+    val localWindowInset = with(LocalDensity.current) {
+        LocalWindowInsets.current.navigationBars.bottom.toDp()
+    }
+
+    Column {
+        BottomNavigation(
+            modifier = Modifier
+                .height(68.dp)
+                .padding(bottom = localWindowInset)
+                .graphicsLayer(
+                    clip = true,
+                    shape = RoundedCornerShape(
+                        topStart = 16.dp,
+                        topEnd = 16.dp,
+                    ),
+                    shadowElevation = 10f,
                 ),
-                shadowElevation = 10f,
-            ),
-        backgroundColor = SafeColor.White,
-    ) {
+            backgroundColor = SafeColor.White,
+        ) {
 
-        val currentDestination = navBackStackEntry?.destination?.route
+            val currentDestination = navBackStackEntry?.destination?.route
 
-        tabs.forEach { tab ->
+            tabs.forEach { tab ->
 
-            val selected = currentDestination == tab.route
+                val selected = currentDestination == tab.route
 
-            BottomNavigationItem(
-                selected = selected,
-                onClick = {
-                    navController.navigate(tab.route)
-                },
-                icon = {
-                    Icon(
-                        modifier = Modifier.padding(
-                            bottom = 8.dp,
-                        ),
-                        painter = painterResource(id = tab.drawableId),
-                        contentDescription = stringResource(id = tab.stringResId),
-                    )
-                },
-                selectedContentColor = SafeColor.Black,
-                unselectedContentColor = SafeColor.Gray500,
-                label = {
-                    Body4(
-                        text = stringResource(id = tab.stringResId),
-                        color = if (selected) SafeColor.Black
-                        else SafeColor.Gray500,
-                    )
-                },
-                alwaysShowLabel = true,
-            )
+                BottomNavigationItem(
+                    selected = selected,
+                    onClick = {
+                        navController.navigate(tab.route)
+                    },
+                    icon = {
+                        Icon(
+                            modifier = Modifier.padding(
+                                bottom = 8.dp,
+                            ),
+                            painter = painterResource(id = tab.drawableId),
+                            contentDescription = stringResource(id = tab.stringResId),
+                        )
+                    },
+                    selectedContentColor = SafeColor.Black,
+                    unselectedContentColor = SafeColor.Gray500,
+                    label = {
+                        Body4(
+                            text = stringResource(id = tab.stringResId),
+                            color = if (selected) SafeColor.Black
+                            else SafeColor.Gray500,
+                        )
+                    },
+                    alwaysShowLabel = true,
+                )
+            }
         }
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(14.dp)
+                .background(color = SafeColor.White)
+        )
     }
 }
